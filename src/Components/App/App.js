ffa075e6b051e61d6db51a0c7c3d612a2c9683b6
ditole <tolerevo@hotmail.com>
2023-12-13 08:32:07 -0600

    end third module

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,30 +1,36 @@
-import React from 'react';
-import styles from './App.module.css';
+import React from "react";
+import styles from "./App.module.css";
 
-import SearchBar from '../SearchBar/SearchBar';
-import BusinessList from '../BusinessList/BusinessList';
+import BusinessList from "../BusinessList/BusinessList";
+import SearchBar from "../SearchBar/SearchBar";
 
 const business = {
-  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
+  imageSrc:
+    "https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg",
+  name: "MarginOtto Pizzeria",
+  address: "1010 Paddington Way",
+  city: "Bordertown",
+  state: "NY",
+  zipCode: "10101",
+  category: "Italian",
   rating: 4.5,
-  reviewCount: 90
+  reviewCount: 90,
 };
+
 const businesses = [business, business, business, business, business, business];
 
 const App = () => {
+  const searchYelp = (term, location, sortBy) => {
+    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
+  };
+
   return (
     <div className={styles.App}>
-     <h1>ravenous</h1>
-     <SearchBar />
-     <BusinessList businesses={businesses} />
+      <h1>ravenous</h1>
+      <SearchBar searchYelp={searchYelp} />
+      <BusinessList businesses={businesses} />
     </div>
   );
-}
+};
 
 export default App;
