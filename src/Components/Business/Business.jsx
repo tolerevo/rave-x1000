6c2abbee6dcd046127efb91954e4a217450ab53d
ditole <tolerevo@hotmail.com>
2023-12-13 03:20:12 -0600

    end business module

--- src/Components/Business/Business.jsx
+++ src/Components/Business/Business.jsx
@@ -1,36 +1,39 @@
 import React from 'react';
 import styles from './Business.module.css';
 
-function Business() {
-  const business = {
-    imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
-    name: 'MarginOtto Pizzeria',
-    address: '1010 Paddington Way',
-    city: 'Flavortown',
-    state: 'NY',
-    zipCode: '10101',
-    category: 'Italian',
-    rating: 4.5,
-    reviewCount: 90
-  }
+const business = {
+  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+};
+
+const Business = () => {
   return (
-    <>
-      <img alt="Marginoto Pizzeria" className={styles.cardImage} src={business.imageSrc} />
-      <h1 className={styles.cardTitle}>{business.name}</h1>
-      <section>
-        <div className={styles.cardInfo}>
+    <div className={styles.Business}>
+      <div className={styles.imageContainer}>
+        <img alt="Marginoto Pizzeria" src={business.imageSrc} />
+      </div>
+      <h2>{business.name}</h2>
+      <div className={styles.BusinessInformation}>
+        <div className={styles.BusinessAddress}>
           <p>{business.address}</p>
           <p>{business.city}</p>
-          <p>{business.state} {business.zipCode}</p>
+          <p>{`${business.state} ${business.zipCode}`}</p>
         </div>
-        <div className={styles.cardInfo}>
-          <p>{business.category}</p>
-          <span className={styles.cardInfoGold}>{business.rating}</span><br />
-          <p className={styles.cardInfoGold}>{business.reviewCount}</p>
+        <div className={styles.BusinessReviews}>
+          <h3>{business.category.toUpperCase()}</h3>
+          <h3 className={styles.rating}>{`${business.rating} stars`}</h3>
+          <p>{`${business.reviewCount} reviews`}</p>
         </div>
-     </section>
-    </>
+      </div>
+    </div>
   );
-}
+};
 
 export default Business;
