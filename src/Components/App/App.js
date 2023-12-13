c9eb766b32ebd4c22b8e2f4d8a02435a6517ec1c
ditole <tolerevo@hotmail.com>
2023-12-13 03:52:06 -0600

    end first part

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,14 @@
-import './App.module.css';
-import Business from '../Business/Business';
+import React from 'react';
+import styles from './App.module.css';
+import SearchBar from '../SearchBar/SearchBar';
+import BusinessList from '../BusinessList/BusinessList';
 
 function App() {
   return (
-    <div className="App">
-     <Business />
+    <div className={styles.App}>
+     <h1>ravenous</h1>
+     <SearchBar />
+     <BusinessList />
     </div>
   );
 }
